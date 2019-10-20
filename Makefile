c447c277c9539be82d2d67144a72c12b21390968
TsimpDim <dimitristsimpitas@gmail.com>
2019-10-20 17:12:20 +0300

    Base

---
+++ Makefile
@@ -0,0 +1,4 @@
+run:
+	export FLASK_APP=_BookSys/__init__.py; \
+	export FLASK_DEBUG=true; \
+	flask run
---
+++ _BookSys/views.py
@@ -0,0 +1,8 @@
+from _BookSys import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+
