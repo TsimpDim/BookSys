e107f3be1efe41fff3511949a39bb9e9b9d0caff
TsimpDim <dimitristsimpitas@gmail.com>
2019-11-05 10:55:04 +0200

    Finalize functionality

--- BookSysPyRO/booksys.py
+++ BookSysPyRO/booksys.py
@@ -1,6 +1,7 @@
 from database import init_db, Book, User, Tracker, get_or_create
 import Pyro4
 import os
+from datetime import datetime
 
 os.environ["PYRO_LOGFILE"] = "pyro.log"
 os.environ["PYRO_LOGLEVEL"] = "DEBUG"
@@ -31,11 +32,23 @@ class BookSys(object):
 
         return "You are now logged out\n"
 
+    def list_info(self):
+        if self.user_session['logged_in']:
+            return {
+                "borrowed": self.db_session.query(Tracker).filter_by(user_id=u.id, returned_at=None).count(),
+                "limit": self.db_session.query(User).filter_by(id=self.user_session['user_id']).first().limit
+            }
+        else:
+            return "You need to login. Use `auth <username>`\n"
+
     def list_books(self):
         return [r.as_dict() for r in self.db_session.query(Book).all()]
 
     def list_trackers(self):
-        return self.db_session.query(Tracker).all()
+        if self.user_session['logged_in']:
+            return [t.as_dict() for t in self.db_session.query(Tracker).filter_by(user_id=self.user_session['user_id'])]
+        else:
+            return "You need to login. Use `auth <username>`\n"
 
     def add_book(self, title, author, description, quantity):
         if self.user_session['logged_in']:
@@ -76,6 +89,60 @@ class BookSys(object):
                 return "You are not the owner of this book\n"
         else:
             return "You need to login. Use `auth <username>`\n"
+
+    def borrow_book(self, book_id):
+        b = self.db_session.query(Book).filter_by(id=book_id).first()
+
+        if self.user_session['logged_in']:
+            if b.owner.username == self.user_session['username']:
+                return "You own this book\n"
+            else:
+                u = self.db_session.query(User).filter_by(id=self.user_session['user_id']).first()
+
+                if self.db_session.query(Tracker).filter_by(user_id=u.id, returned_at=None).count() == u.limit:
+                    return "Borrowing quota 100%, return some books\n"
+
+                if self.db_session.query(Tracker).filter_by(user_id=u.id, book_id=b.id, returned_at=None).count() > 0:
+                    return "You have already borrowed this book"
+
+                if b.quantity == 0:
+                    return "There are no copies left :("
+
+                t = Tracker(book_id=b.id, user_id=u.id)
+
+                b.quantity -= 1
+                self.db_session.add(t)
+                self.db_session.commit()
+
+                return "Borrowed book"
+        else:
+            return "You need to login. Use `auth <username>`\n"
+
+    def return_book(self, book_id):
+        b = self.db_session.query(Book).filter_by(id=book_id).first()
+        u = self.db_session.query(User).filter_by(id=self.user_session['user_id']).first()
+
+        if not b:
+            # Object has been deleted
+            t = self.db_session.query(Tracker).filter_by(user_id=u.id, book_id=None).order_by(Tracker.id.desc()).first()
+            t.returned_at = datetime.now()
+
+            self.db_session.commit()
+            return "Book was deleted. 'Returned' successfully.\n"
+
+        if self.user_session['logged_in']:
+            if b.owner.username == self.user_session['username']:
+                return "You own this book\n"
+            else:
+                t = self.db_session.query(Tracker).filter_by(book_id=b.id, user_id=u.id).order_by(Tracker.id.desc()).first()
+                b.quantity += 1
+                t.returned_at = datetime.now()
+
+                self.db_session.commit()
+                return "Book returned successfully.\n"
+        else:
+            return "You need to login. Use `auth <username>`\n"
+ 
 
 def main():
     bs = BookSys()
--- BookSysPyRO/client.py
+++ BookSysPyRO/client.py
@@ -27,9 +27,34 @@ while(True):
                 print("[{id}] {title:35} {author:20} {description:60} {quantity}".format(**book))
 
         elif inp[1] == "trackers":
-            res = booksys.list_trackers()
-            print(res)
-    
+            if not logged_in:
+                print("You need to login. Use `auth <username>`")
+            else:
+                res = booksys.list_trackers()
+
+                for t in res:
+                    if not t['returned_at']: t['returned_at'] = '---'
+
+                print("{i:10} {u_i:10} {b_i:10} {b_a:15} {r_a:10}".format(i="id", u_i="user_id", b_i="book_id", b_a="borrowed_at", r_a="returned_at"))
+                for t in res:
+                    print("{id:<10} {user_id:<10} {book_id:<10} {borrowed_at:15} {returned_at:10}".format(**t))    
+
+        elif inp[1] == "info":
+            if not logged_in:
+                print("You need to login. Use `auth <username>`")
+            else:
+                print(booksys.list_info())
+
+        elif inp[1] == "commands":
+            print("Available commands:")
+            print("* auth <username>")
+            print("* unauth")
+            print("* list <books|trackers|commands>")
+            print("* add book")
+            print("* delete book <book_id>")
+            print("* borrow book <book_id>")
+            print("* return book <book_id>\n")
+
     elif inp == ["add", "book"]:
         if not logged_in:
             print("You need to login. Use `auth <username>`")
@@ -58,9 +83,35 @@ while(True):
             id = int(inp[2])
 
             res = booksys.delete_book(id)
-            
+            print(res)
+
+    elif inp[:2] == ["borrow", "book"]:
+        if len(inp) == 2:
+            print("No book_id specified. Command is `borrow book <book_id>`")
+
+        if not logged_in:
+            print("You need to login. Use `auth <username>`")
+        else:
+            id = int(inp[2])
+
+            res = booksys.borrow_book(id)
+            print(res)
+
+    elif inp[:2] == ["return", "book"]:
+        if len(inp) == 2:
+            print("No book_id specified. Command is `borrow book <book_id>`")
+
+        if not logged_in:
+            print("You need to login. Use `auth <username>`")
+        else:
+            id = int(inp[2])
+
+            res = booksys.return_book(id)
             print(res)
 
     elif inp == ["quit"]:
         exit()
+
+    else:
+        print("Use command 'list commands' to see all available commands")
 
